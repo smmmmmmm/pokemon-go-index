14d603e95ccecca442ffd46a7b660bac17da684c
smmmmmmm <102597242+smmmmmmm@users.noreply.github.com>
2023-07-28 00:55:05 +0900

    emlator 周りの設定を修正 (#7)

--- frontend/src/infra/firebase.ts
+++ frontend/src/infra/firebase.ts
@@ -6,18 +6,43 @@ import {
   getFirestore,
 } from "firebase/firestore/lite";
 
-const app = initializeApp({
-  projectId: 'emulator',
-  appId: 'test',
-  apiKey: 'test',
-});
-const db = getFirestore(app);
-const auth = getAuth(app);
+const initializeFirebase = () => {
+  const app = initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  });
+  const db = getFirestore(app);
+  const auth = getAuth(app);
+  return { db, auth };
+};
 
-if (process.env.NEXT_PUBLIC_EMULATOR === "true") {
+const initializeEmulatorFirebase = () => {
+  const app = initializeApp({
+    projectId: "demo-emulator",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    appId: "test",
+    apiKey: "test",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    measurementId: "xxx",
+  });
+  const db = getFirestore(app);
+  const auth = getAuth(app);
   connectAuthEmulator(auth, "http://localhost:9099");
   connectFirestoreEmulator(db, "localhost", 8080);
-}
+  return { db, auth };
+};
+
+const { db, auth } = process.env.NEXT_PUBLIC_EMULATOR
+  ? initializeEmulatorFirebase()
+  : initializeFirebase();
 
 export const login = async () => {
   const provider = new GoogleAuthProvider();
