79872302077e1e9b9f696fcf286264cf136fd357
smmmmmmm <simasima442317@gmail.com>
2023-08-20 18:52:08 +0900

    tmp update circle

--- frontend/src/components/uiParts/Circle/Circle.stories.tsx
+++ frontend/src/components/uiParts/Circle/Circle.stories.tsx
@@ -35,6 +35,19 @@ export const Full: StoryObj<T> = {
     progress: 100,
     myCount: 100,
     allCount: 100,
+    lineWidth: "40",
+    animate: true,
+    animationDuration: "1s",
+    bgColor: "#ddd",
+  },
+};
+
+export const Zero: StoryObj<T> = {
+  args: {
+    label: "label",
+    progress: 0,
+    myCount: 0,
+    allCount: 100,
     lineWidth: "25",
     animate: true,
     animationDuration: "1s",
